3bef0d97895c7dd3d6396beb64395cc9e8bea9ed
Hongyi Chen <59629242+hychen-naza@users.noreply.github.com>
2020-02-08 13:59:53 -0500

    Update Education.js

--- Education.js
+++ Education.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+
 import "Education.css";
 
-class Education extends Component {
-  render() {
-    return (
 
-      <!-- Education -->
-      <div id="education" className="Education section">
-        <h1>Education</h1>
-        <div className="column">
-          <h2>Carnegie Mellon University</h2>
-          <h3>M.S. in Electrical and Computer Engineering | Expected in December 2020</h3>
-          <p>Tentative schedule: Advanced Introduction to Machine Learning, Introduction to Deep Learning, Introduction to Entrepreneurship, etc.</p>
 
-          <h3>B.S. in Electrical and Computer Engineering | May, 2018</h3>
-          <h4>3.68/4.0 with College and University Honor</h4>
-          <p>Courses: Introduction to Machine Learning, Deep Reinforcement Learning and Control, Natural Language Processing, etc.</p>
-        </div>
-      </div>
-    );
-  }
-}
+<!-- Education -->
+<div id="education" className="Education section">
+  <h1>Education</h1>
+  <div className="column">
+    <h2>Carnegie Mellon University</h2>
+    <h3>M.S. in Electrical and Computer Engineering | Expected in December 2020</h3>
+    <p>Tentative schedule: Advanced Introduction to Machine Learning, Introduction to Deep Learning, Introduction to Entrepreneurship, etc.</p>
 
-export default Education;
+    <h3>B.S. in Electrical and Computer Engineering | May, 2018</h3>
+    <h4>3.68/4.0 with College and University Honor</h4>
+    <p>Courses: Introduction to Machine Learning, Deep Reinforcement Learning and Control, Natural Language Processing, etc.</p>
+  </div>
+</div>
+
+
